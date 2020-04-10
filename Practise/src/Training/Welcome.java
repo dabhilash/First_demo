623784ebef173f4307c804257d00cd90710e2620
Abhilash <you@example.com>
2020-04-10 12:36:00 +0530

    First commit

---
+++ Practise/src/Training/Welcome.java
@@ -0,0 +1,14 @@
+package Training;
+
+public class Welcome {
+
+	public static void main(String[] args) 
+	{
+		System.out.println("Welcome Samhika");
+		int a=10,b=20;
+		int c;
+		c=a+b;
+		System.out.println(c);
+	}
+
+}
---
+++ Practise/src/Training/multiplication.java
@@ -0,0 +1,14 @@
+package Training;
+
+public class multiplication {
+
+	public static void main(String[] args) 
+	{
+		int a=10,b=10;
+		int c;
+		c=a*b;
+		System.out.println(c);
+
+	}
+
+}
